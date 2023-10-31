97a43089f216cc46d25b5a9fcf253daa781fd6ff
luis-designs <luislozano120xd@gmail.com>
2023-10-31 17:28:25 -0500

    :pen: Cors

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 app.use(express.json({ limit: '50mb' }));
 app.use(
   express.urlencoded({
